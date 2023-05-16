803e264c79710ec6b0d3dcfe9b1e97ee1f44c8ea
MadeByAIBots <madebyaibots@gmail.com>
2023-05-16 07:09:29 +0000

    Update setup script to install peft

--- setup.sh
+++ setup.sh
@@ -7,3 +7,6 @@ sudo apt-get install -y linux-tools-common linux-tools-generic linux-tools-`unam
 sudo apt-get install -y python3.8 python3-pip
 pip3 install --upgrade pip
 
+# Install peft
+pip3 install peft
+
