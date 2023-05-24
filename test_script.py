da00ab6ab44e9856a1153faeb9bfbf7e97db5e3a
MadeByAIBots <madebyaibots@gmail.com>
2023-05-24 14:27:07 +0000

    Add test script

---
+++ test_script.py
@@ -0,0 +1,20 @@
+
+import os
+from trainer import Trainer
+
+def main():
+    # Instantiate the Trainer class
+    trainer = Trainer()
+
+    # Load the model
+    model_name = 'gpt2'
+    trainer.load_model(model_name)
+
+    # Prepare the training text
+    training_text = 'Human: What is the weather like today? Assistant: It is sunny.'
+
+    # Train the model on the training text
+    trainer.train(training_text)
+
+if __name__ == '__main__':
+    main()
