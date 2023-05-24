65d05ecabfeebf6bb413a620e8f3da15b1ead543
MadeByAIBots <madebyaibots@gmail.com>
2023-05-24 14:52:36 +0000

    Add data, test prompt and response text files and update test_script.py

--- test_script.py
+++ test_script.py
@@ -17,5 +17,22 @@ def main():
     # Train the model on the training text
     trainer.train(training_text)
 
+    # Prepare the test prompt
+    with open('test-prompt.txt', 'r') as f:
+        test_prompt = f.read()
+
+    # Generate a response from the model
+    response = trainer.generate_response(test_prompt)
+    print('Generated response:', response)
+
+    # Prepare the expected response
+    with open('test-response.txt', 'r') as f:
+        expected_response = f.read()
+
+    # Assert that the response contains the expected response
+    assert expected_response in response, f'Expected "{expected_response}" to be in "{response}"'
+
+    print('Test passed.')
+
 if __name__ == '__main__':
     main()
